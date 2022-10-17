4236d010e0390396b734c3ce463672e375e82afb
Angela Cui <ayc62@cornell.edu>
2022-10-17 00:22:28 -0400

    printboard functions

--- src/controller.ml
+++ src/controller.ml
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-=======
-(* Note: You may introduce new code anywhere in this file. *)
->>>>>>> 571312e761631a2cc18ec41051ec10ead26958db
 open Board
 
 let init_state =
   [
-<<<<<<< HEAD
     ("a1", { piece_type = Rook; piece_color = White });
     ("b1", { piece_type = Knight; piece_color = White });
     ("c1", { piece_type = Bishop; piece_color = White });
@@ -40,38 +35,3 @@ let init_state =
     ("g8", { piece_type = Knight; piece_color = Black });
     ("h8", { piece_type = Rook; piece_color = Black });
   ]
-=======
-    { pos = "a1"; piece = Rook; color = White };
-    { pos = "b1"; piece = Knight; color = White };
-    { pos = "c1"; piece = Bishop; color = White };
-    { pos = "d1"; piece = Queen; color = White };
-    { pos = "e1"; piece = King; color = White };
-    { pos = "f1"; piece = Bishop; color = White };
-    { pos = "g1"; piece = Knight; color = White };
-    { pos = "h1"; piece = Rook; color = White };
-    { pos = "a2"; piece = Pawn; color = White };
-    { pos = "b2"; piece = Pawn; color = White };
-    { pos = "c2"; piece = Pawn; color = White };
-    { pos = "d2"; piece = Pawn; color = White };
-    { pos = "e2"; piece = Pawn; color = White };
-    { pos = "f2"; piece = Pawn; color = White };
-    { pos = "g2"; piece = Pawn; color = White };
-    { pos = "h2"; piece = Pawn; color = White };
-    { pos = "a8"; piece = Rook; color = Black };
-    { pos = "b8"; piece = Knight; color = Black };
-    { pos = "c8"; piece = Bishop; color = Black };
-    { pos = "d8"; piece = Queen; color = Black };
-    { pos = "e8"; piece = King; color = Black };
-    { pos = "f8"; piece = Bishop; color = Black };
-    { pos = "g8"; piece = Knight; color = Black };
-    { pos = "h8"; piece = Rook; color = Black };
-    { pos = "a7"; piece = Pawn; color = Black };
-    { pos = "b7"; piece = Pawn; color = Black };
-    { pos = "c7"; piece = Pawn; color = Black };
-    { pos = "d7"; piece = Pawn; color = Black };
-    { pos = "e7"; piece = Pawn; color = Black };
-    { pos = "f7"; piece = Pawn; color = Black };
-    { pos = "g7"; piece = Pawn; color = Black };
-    { pos = "h7"; piece = Pawn; color = Black };
-  ]
->>>>>>> 571312e761631a2cc18ec41051ec10ead26958db
---
+++ src/printboard.ml
@@ -0,0 +1,22 @@
+open Board
+
+let piece_type_string piece = match piece.piece_type with 
+| Rook -> "R"
+| Knight -> "N"
+| Bishop -> "B"
+| Queen -> "Q"
+| King -> "K"
+| Pawn -> "P"
+
+let piece_color_string piece = match piece.piece_color with
+  | White -> "w"
+  | Black -> "b"
+
+let piece state square = try (let piece = List.assoc square state in 
+  ((piece_color_string piece) ^ (piece_type_string piece)))
+  with | e -> "  " 
+
+let square_pos (row:int) (col:char) = (Char.escaped col) ^ (string_of_int row)
+
+let rec print_row state row col = "| " ^ (if col = 'a' then piece state (square_pos row col) 
+  else print_row state row (col |> Char.code |> ( - ) 1 |> Int.abs |> Char.chr))
