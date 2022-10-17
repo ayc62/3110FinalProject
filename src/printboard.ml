d7912f5c0b02bff626ed91729fd4310307599bc5
Angela Cui <ayc62@cornell.edu>
2022-10-17 00:32:57 -0400

    fixed print_row in printboard

--- src/printboard.ml
+++ src/printboard.ml
@@ -14,9 +14,11 @@ let piece_color_string piece = match piece.piece_color with
 
 let piece state square = try (let piece = List.assoc square state in 
   ((piece_color_string piece) ^ (piece_type_string piece)))
-  with | e -> "  " 
+  with | e -> "   " 
 
 let square_pos (row:int) (col:char) = (Char.escaped col) ^ (string_of_int row)
 
-let rec print_row state row col = "| " ^ (if col = 'a' then piece state (square_pos row col) 
-  else print_row state row (col |> Char.code |> ( - ) 1 |> Int.abs |> Char.chr))
+let rec print_row state row col = "| " ^ (piece state (square_pos row col)) ^ " " ^
+  if col = 'h' then "|" else
+  print_row state row (col |> Char.code |> ( + ) 1 |> Int.abs |> Char.chr)
+
