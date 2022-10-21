c40cea01c177469b5d9f8e03e8db3ac5abfa7ad3
slw259 <slw259@cornell.edu>
2022-10-21 01:17:59 -0400

    Partially implemented an .mli file for board with incomplete abstraction

---
+++ src/board.mli
@@ -0,0 +1,56 @@
+type piece_type =
+  | Rook
+  | Knight
+  | Bishop
+  | Queen
+  | King
+  | Pawn  (**The abstract type representing what type a piece is*)
+
+type piece_color =
+  | White
+  | Black  (**The abstract type representing the color of a piece*)
+
+type piece_state = {
+  piece_type : piece_type;
+  piece_color : piece_color;
+  moved : bool;
+}
+(**The abstract type representing a piece*)
+
+val piece_helper :
+  ?moved:bool -> string -> piece_type -> piece_color -> string * piece_state
+(**[piece_helper] takes in the position [pos] of a piece, and the type [p_type]
+   and color [p_color] of the piece, and returns it in an association list entry
+   format. [pos] is the key, and the value is a record with [p_type] and
+   [p_color].*)
+
+val get_piece_type : piece_state -> piece_type
+(**[get_piece_type] returns the type of the piece*)
+
+val get_piece_color : piece_state -> piece_color
+(**[get_piece_color] returns the color of the piece*)
+
+val get_moved : piece_state -> bool
+(**[get_moved] returns whether or not the piece has been moved*)
+
+type board = (string * piece_state) list
+(**The abstract type representing the chessboard*)
+
+type state = {
+  board : (string * piece_state) list;
+  old_boards : board list;
+  fifty_move_rule : int;
+}
+(**The abstract type representing the current and past states of the chessboard*)
+
+val get_board : state -> (string * piece_state) list
+(**[get_board] returns the chessboard*)
+
+val get_old_boards : state -> board list
+(**[get_old_boards] returns the chessboards in previous moves*)
+
+val get_fifty_move_rule : state -> int
+(**[get_fifty_move_rule] returns the number of moves since last capture*)
+
+val init_state : state
+(**[init_state] is the initial state of the chessboard*)
